062b7cc008298bcda8310ab2a9e33e7d23c3f04a
zhichenghe2008 <zhichenghe@deeproute.ai>
2023-06-24 17:22:38 +0800

    test

---
+++ copy.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/en.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/locales/en.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/cn.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/locales/cn.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/chat.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/components/chat.tsx
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/sidebar.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/components/sidebar.tsx
+
+
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/en.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web2/myapp2/app/locales/en.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/cn.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web2/myapp2/app/locales/cn.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/chat.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web2/myapp2/app/components/chat.tsx
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/sidebar.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web2/myapp2/app/components/sidebar.tsx
+
+
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/en.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web3/myapp3/app/locales/en.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/cn.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web3/myapp3/app/locales/cn.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/chat.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web3/myapp3/app/components/chat.tsx
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/sidebar.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web3/myapp3/app/components/sidebar.tsx
+
+
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/en.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/locales/en.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/cn.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/locales/cn.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/chat.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/components/chat.tsx
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/sidebar.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/components/sidebar.tsx
+
+
+
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/en.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/locales/en.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/cn.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/locales/cn.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/chat.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/components/chat.tsx
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/sidebar.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/components/sidebar.tsx
+
+
+
+
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/en.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web6/myapp6/app/locales/en.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/cn.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web6/myapp6/app/locales/cn.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/chat.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web6/myapp6/app/components/chat.tsx
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/sidebar.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web6/myapp6/app/components/sidebar.tsx
+
+
+
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/en.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web7/myapp7/app/locales/en.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/cn.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web7/myapp7/app/locales/cn.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/chat.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web7/myapp7/app/components/chat.tsx
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/sidebar.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web7/myapp7/app/components/sidebar.tsx
+
+
+
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/en.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web8/myapp8/app/locales/en.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/cn.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web8/myapp8/app/locales/cn.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/chat.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web8/myapp8/app/components/chat.tsx
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/sidebar.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web8/myapp8/app/components/sidebar.tsx
+
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/en.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web9/myapp9/app/locales/en.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/locales/cn.ts /home/DEEPROUTE/zhichenghe/project/chatgpt/web9/myapp9/app/locales/cn.ts
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/chat.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web9/myapp9/app/components/chat.tsx
+cp /home/DEEPROUTE/zhichenghe/project/chatgpt/web/ChatGPT-Next-Web/app/components/sidebar.tsx /home/DEEPROUTE/zhichenghe/project/chatgpt/web9/myapp9/app/components/sidebar.tsx
+
+
+
+
+
+
+#更改合作站点配置
+#web5 ai.chatgptvip.info
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/locales/en.ts|grep h285426134
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/locales/cn.ts|grep h285426134
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/components/sidebar.tsx|grep h285426134
+
+sed -i 's/h285426134/longlielie/g' /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/locales/en.ts
+sed -i 's/h285426134/longlielie/g' /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/locales/cn.ts
+sed -i 's/h285426134/longlielie/g' /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/components/sidebar.tsx
+
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/locales/en.ts|grep longlielie
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/locales/cn.ts|grep longlielie
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web5/myapp5/app/components/sidebar.tsx|grep longlielie
+
+
+#web4  web.chatgptvip.info
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/locales/en.ts|grep h285426134
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/locales/cn.ts|grep h285426134
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/components/sidebar.tsx|grep h285426134
+
+sed -i 's/h285426134/djun138/g' /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/locales/en.ts
+sed -i 's/h285426134/djun138/g' /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/locales/cn.ts
+sed -i 's/h285426134/djun138/g' /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/components/sidebar.tsx
+
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/locales/en.ts|grep djun138
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/locales/cn.ts|grep djun138
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web4/myapp4/app/components/sidebar.tsx|grep djun138
+
+
+#web1  ai.chatgptvip.shop
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/locales/en.ts|grep h285426134
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/locales/cn.ts|grep h285426134
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/components/sidebar.tsx|grep h285426134
+
+sed -i 's/h285426134/harrylee_007/g' /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/locales/en.ts
+sed -i 's/h285426134/harrylee_007/g' /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/locales/cn.ts
+sed -i 's/h285426134/harrylee_007/g' /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/components/sidebar.tsx
+
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/locales/en.ts|grep harrylee_007
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/locales/cn.ts|grep harrylee_007
+cat /home/DEEPROUTE/zhichenghe/project/chatgpt/web1/myapp1/app/components/sidebar.tsx|grep harrylee_007
